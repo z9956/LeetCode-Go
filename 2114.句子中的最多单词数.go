97e0c5e0b135701b600454573ef63620e513cf46
z9956 <zhz995617@gmail.com>
2022-01-05 23:43:26 +0800

    commit

---
+++ 2114.句子中的最多单词数.go
@@ -0,0 +1,14 @@
+package leetcode
+
+import "strings"
+
+func mostWordsFound(sentences []string) (max int) {
+	for _, sentence := range sentences {
+		count := strings.Count(sentence, " ") + 1
+
+		if count > max {
+			max = count
+		}
+	}
+	return max
+}
