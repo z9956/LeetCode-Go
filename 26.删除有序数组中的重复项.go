fe0c0318993f759053d97ca07f601fda7f76cf8d
zhz995617@gmail.com <zhangzhi@tolead.com>
2022-01-07 09:42:26 +0800

    commit

---
+++ 26.删除有序数组中的重复项.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func removeDuplicates(nums []int) int {
+	length := len(nums)
+	if length == 0 {
+		return 0
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+
+	return i + 1
+}
