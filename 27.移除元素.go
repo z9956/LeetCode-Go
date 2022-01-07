8668243fab38047c0a8a1a42a6944bb307449461
zhz995617@gmail.com <zhangzhi@tolead.com>
2022-01-07 18:00:37 +0800

    commit

---
+++ 27.移除元素.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func removeElement(nums []int, val int) int {
+	i := 0
+
+	for i < len(nums) {
+		if nums[i] == val {
+			nums = append(nums[:i], nums[i+1:]...)
+		} else {
+			i++
+		}
+	}
+
+	return i
+}
