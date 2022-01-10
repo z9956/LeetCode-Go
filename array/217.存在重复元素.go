0e1ce5f4c2ce8bc698154293ce7368f551fdfe1e
z9956 <zhz995617@gmail.com>
2022-01-10 13:11:07 +0800

    commit

---
+++ array/217.存在重复元素.go
@@ -0,0 +1,15 @@
+package array
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]bool)
+
+	for i := 0; i < len(nums); i++ {
+		if m[nums[i]] {
+			return true
+		}
+
+		m[nums[i]] = true
+	}
+
+	return false
+}
