83403d6a3924b7734e351be619a213b0014ce7d3
z9956 <zhz995617@gmail.com>
2022-01-20 16:24:14 +0800

    commit

--- hash-Table/13.罗马数字转整数.go
+++ hash-Table/13.罗马数字转整数.go
@@ -1,1 +1,25 @@
 package hash_Table
+
+func romanToInt(s string) (sum int) {
+	table := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	for i := range s {
+		cur := table[s[i]]
+
+		if i < len(s)-1 && cur < table[s[i+1]] {
+			sum -= cur
+		} else {
+			sum += cur
+		}
+	}
+
+	return sum
+}
